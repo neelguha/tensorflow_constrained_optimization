b79155713bbcfc559a2289c4216c7ad3810d6a72
Neel Guha <neelguha@gmail.com>
2019-11-14 00:55:18 -0500

    Update castor_init.sh

--- experiments/castor_init.sh
+++ experiments/castor_init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # bash file for Neel when running local scripts on his machine 
-export fairness_data="/Users/neelguha/Dropbox/NeelResearch/fairness/data/"
-export results_dir="/Users/neelguha/Dropbox/NeelResearch/fairness/code/results/"
+export fairness_data="//home/nguha/fairness/data/"
+export results_dir="/home/nguha/fairness/results"
