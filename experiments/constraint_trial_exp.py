190d222a2ca356ae918d792f8a42bf19722e7f42
Neel Guha <neelguha@gmail.com>
2019-11-25 22:19:16 -0800

    Update constraint_trial_exp.py

--- experiments/constraint_trial_exp.py
+++ experiments/constraint_trial_exp.py
@@ -46,8 +46,8 @@ def main():
             active_protected = np.random.choice(all_protected_columns, num_protected, replace = False)
             train_violation, train_rates, test_violation, test_rates, scores = run_eo_experiment( 
                 train_df, test_df, feature_names, label_column, 
-                active_protected, all_protected_columns, epochs=10, 
-                minibatch_size=128,  max_diff=0.05, lr=0.005
+                active_protected, all_protected_columns, epochs=5, 
+                minibatch_size=128,  max_diff=0.05, lr=0.01
             )
             trial_results[k] = {
                 'train_violation': train_violation, 
